14280185a7dca1688f38b6a44616b83639a06035
Legacy Vault <chelovek.2k@yandex.ru>
2019-12-13 21:46:35 +0300

    C# LINQ Usage with MySQL Database.

---
+++ C#/LINQ/LinqTest2/cs/ClassLibrary/ProductSalesContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+//using MySql.Data.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using System;
+
+namespace ClassLibrary
+{
+	public class ProductSalesContext : DbContext
+	{
+		public ProductSalesContext() : base() { }
+		public ProductSalesContext(DbContextOptions<ProductSalesContext> options) : base(options) { }
+
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Sale> Sales { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			string dsn = "server=127.0.0.1;uid=test;pwd=test;database=test";
+			Action<MySqlDbContextOptionsBuilder> mySqlOptionsAction = o => o.EnableRetryOnFailure().CommandTimeout(60);
+			optionsBuilder.UseMySql(dsn, mySqlOptionsAction);
+		}
+	}
+}
