7aea98d39a800833aaa23a218d1242b661e3bdb2
Legacy Vault <chelovek.2k@yandex.ru>
2020-01-04 01:19:19 +0300

    Simple HTTP Server Example.

---
+++ C#/http/HttpListenerUsage/HttpListenerUsage/Program.cs
@@ -0,0 +1,17 @@
+﻿using HttpServerLibrary;
+using System.Threading;
+
+namespace HttpListenerUsage
+{
+	class Program
+	{
+		static void Main()
+		{
+			Server server = new Server("http", "127.0.0.1", "3000");
+			bool ok = server.Start();
+			if (!ok) return;
+			Thread.Sleep(15_000);
+			server.Stop();
+		}
+	}
+}
